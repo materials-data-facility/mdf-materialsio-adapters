b2d030c5ddab771b643e2e3ceb2a3abb9753fb14
Logan Ward <ward.logan.t@gmail.com>
2019-07-29 12:27:07 -0500

    Added test for search index function
    Need to actually look at the coverage

---
+++ tests/test_search_index.py
@@ -0,0 +1,12 @@
+"""Tests for the key 'make search index' function"""
+
+from mdf_matio import generate_search_index
+import os
+
+
+file_dir = os.path.join(os.path.dirname(__file__), '..', 'notebooks', 'example-files')
+
+
+def test_parse():
+    records = list(generate_search_index(file_dir, False))
+    assert len(records) == 2
