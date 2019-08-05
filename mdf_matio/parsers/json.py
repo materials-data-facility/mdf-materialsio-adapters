fc9b3264113e1c860492d333a9336e13769b248f
Logan Ward <ward.logan.t@gmail.com>
2019-08-05 14:06:06 -0500

    Added JSON parser

---
+++ mdf_matio/parsers/json.py
@@ -0,0 +1,27 @@
+from materials_io.base import BaseSingleFileParser
+from typing import List
+import json
+
+
+class JSONParser(BaseSingleFileParser):
+    """Parses both JSON and line-delimited JSON files"""
+
+    def _parse_file(self, path: str, context=None):
+        try:
+            with open(path) as fp:
+                content = json.load(fp)
+            return {'line-delimited': False, 'content': content}
+        except json.decoder.JSONDecodeError as e:
+            if not str(e).startswith('Extra data: line 2 column 1'):
+                raise e
+
+        # The file could be a line-delimited JSON file
+        with open(path) as fp:
+            content = [json.loads(line) for line in fp]
+        return {'line-delimited': True, 'content': content}
+
+    def implementors(self) -> List[str]:
+        return ['Logan Ward']
+
+    def version(self) -> str:
+        return '0.0.1'
