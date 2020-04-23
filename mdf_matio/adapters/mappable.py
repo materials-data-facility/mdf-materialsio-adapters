8b201773f328f52d7933a79875c47c1c7ee26802
jgaff <jgaff@uchicago.edu>
2020-04-23 12:46:52 -0500

    Delete redundant JSONParser and JSONAdapter

--- mdf_matio/adapters/mappable.py
+++ mdf_matio/adapters/mappable.py
@@ -1,7 +1,6 @@
 """Adapters for structured files"""
 from materials_io.adapters.base import BaseAdapter
 from typing import List, Tuple, Union
-import jmespath
 
 
 def _add_value(record, key: Tuple[str], value):
@@ -47,51 +46,6 @@ class CSVAdapter(BaseAdapter):
             for col, key in col_to_mdf.items():
                 if col in record:
                     _add_value(new_record, key, record[col])
-            if len(new_record) > 0:
-                sub_records.append(new_record)
-        return sub_records if len(sub_records) > 0 else None
-
-
-class JSONAdapter(BaseAdapter):
-    """Converts JSON records into MDF format
-
-    The JSON adapter requires a single context parameter: ``mapping``.
-    Mapping defines the name of the MDF field (using '.'s to separate keys at different levels)
-    to a JMESPath expression definine the location of the desired value(s).
-    """
-
-    def transform(self, metadata: dict,
-                  context: Union[None, dict] = None) -> Union[None, List[dict]]:
-        # We cannot handle JSON files if the user does not define a mapping
-        if context is None:
-            return None
-        if 'mapping' not in context:
-            return None
-
-        # Get the "n/a" values
-        na_values = context.get('na_values', [])
-        if not isinstance(na_values, list):
-            na_values = [na_values]
-
-        # Compile the JMESPath expressions
-        mdf_to_jmes = dict((tuple(x.split('.')), jmespath.compile(y))
-                           for x, y in context['mapping'].items())
-
-        # Get the list of records
-        if metadata['line-delimited']:
-            records = metadata['content']
-        else:
-            content = metadata['content']
-            records = content if isinstance(content, list) else [content]
-
-        # Generate entries
-        sub_records = []
-        for record in records:
-            new_record = {}
-            for key, expr in mdf_to_jmes.items():
-                value = expr.search(record)
-                if value is not None and value not in na_values:
-                    _add_value(new_record, key, value)
             if len(new_record) > 0:
                 sub_records.append(new_record)
         return sub_records if len(sub_records) > 0 else None
